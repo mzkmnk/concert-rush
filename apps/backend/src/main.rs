3abb2c37244f5f74d61af8aeaa034da629ebae87
mzkmnk <mzk.mnk.dev@gmail.com>
2026-01-12 22:16:15 +0900

    feat: run health check

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+    Json,
+};
+use serde_json::{Value, json};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health",get(health_check));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener,app).await.unwrap();
+}
+
+async fn health_check() -> Json<Value> {
+    Json(json!({"status": "ok"}))
 }
