0136e2d148b7a755fe7f5c8415b0782f507095cb
m4i <mzk.mnk.dev@gmail.com>
2026-01-18 22:44:46 +0900

    feat: connect tidb (#5)

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
+import { configModule } from './utils/config';
+import { typeOrmModule } from './utils/typeorm';
 
 @Module({
-  imports: [HealthModule],
+  imports: [configModule, typeOrmModule, HealthModule],
 })
 export class AppModule {}
